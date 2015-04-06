26e7e6a9a157443749a544ac849520afbd5c6862
XhmikosR <xhmikosr@gmail.com>
2015-04-06 10:13:25 +0300

    Simplify JSHint's options.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         jshintrc: true
       },
       files: {
-        src: ['*.js', 'tasks/*.js']
+        src: ['Gruntfile.js', 'tasks/*.js']
       }
     },
     connect: {
